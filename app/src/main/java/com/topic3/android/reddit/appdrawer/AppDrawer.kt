a4d75e91c7d97fbcb8fdb8c525a376cc00276c36
nastyapere <pavevaa1@mail.ru>
2023-04-02 18:43:06 +0300

    inf profile

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -3,10 +3,8 @@ package com.topic3.android.reddit.appdrawer
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ShoppingCart
@@ -21,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.topic3.android.reddit.R
 
 import com.topic3.android.reddit.theme.RedditThemeSettings
@@ -82,7 +81,37 @@ private fun ProfileInfoItem(
   textResourceId: Int,
   modifier: Modifier
 ) {
-  //TODO add your code here
+ val color = colors
+
+  ConstraintLayout(modifier = modifier) {
+    val (iconRef, amountRef, titleRef) = createRefs()
+    val itemModifier = Modifier
+
+    Icon(
+      contentDescription = stringResource(id = textResourceId),
+      imageVector = iconAsset,
+      tint = Color.Blue,
+      modifier = itemModifier
+    )
+    Text(
+      text = stringResource(amountResourceId),
+      color = colors.primaryVariant,
+      fontSize = 10.sp,
+      modifier = itemModifier
+    )
+            Text(
+              text = stringResource(amountResourceId),
+              color = Color.Gray,
+              fontSize = 10.sp,
+              modifier = itemModifier
+        .padding(start = 8.dp)
+        .constrainAs(titleRef) {
+          top.linkTo(amountRef.top)
+          start.linkTo(iconRef.end)
+          bottom.linkTo(iconRef.top)
+        }
+    )
+  }
 }
 
 /**
